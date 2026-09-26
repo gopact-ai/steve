83ab92fa7c34e77e34c5bb822ae5a0c50b43c457
LPX-E5BD8 <lipengxiang_@outlook.com>
2026-09-26 14:49:34 +0800

    build(console): rebuild the embedded console

--- internal/readmodel/web/dist/assets/swimlanesDiagram-TC7HE7FX-DC5dMu8w.js
+++
@@ -1,8 +0,0 @@
-import"./chunk-ZIGJFQKS-Bfe3v5g-.js";import"./src-B4ble-Uq.js";import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./chunk-O7XYJQB3-PKD_DggL.js";import"./chunk-742MDFTN-CBL7qVnV.js";import"./chunk-MBY4JIJT-B6gW2psz.js";import"./chunk-7PRAP22T-BAKX9NmC.js";import"./chunk-7INBJB4K-B7K6LHCA.js";import"./chunk-5DYCD2WN-HrL-yx2L.js";import"./chunk-Z7XXMR3K-CbRm9HI9.js";import"./chunk-GWA4HPMP-DjPBvHQG.js";import"./chunk-XXDRQBXY-DGS6eoxP.js";import"./chunk-WEXAMYUT-BMoJui9U.js";import"./chunk-UA2S7LBM-C1unxGwM.js";import{r as t,t as n}from"./chunk-7M6MHVWA-WZBnNMQC.js";import"./mermaid.core-psrcxsjb.js";var r=n({styles:e(e=>`${t(e)}
-  .swimlane.cluster:not([data-color-id]) rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
